09b50ceceb7e35eb032c96d124d188ee89090116
Harshil Kachhadiya <harshil23kachhadiya@gmail.com>
2026-09-13 01:08:14 +0530

    update-autofill-settings

--- extension/content.js
+++ extension/content.js
@@ -53,12 +53,12 @@ chrome.runtime.onMessage.addListener((message, _sender, sendResponse) => {
 
     let filled = 0;
 
-    if (usernameField && message.username && (!message.autoFill || !usernameField.value)) {
+    if (usernameField && message.username) {
       setNativeValue(usernameField, message.username);
       filled++;
     }
 
-    if (passwordField && message.password && (!message.autoFill || !passwordField.value)) {
+    if (passwordField && message.password) {
       setNativeValue(passwordField, message.password);
       filled++;
     }
@@ -80,7 +80,7 @@ chrome.runtime.onMessage.addListener((message, _sender, sendResponse) => {
     const usernameField = findUsernameField(passwordField);
 
     let filledUsername = false;
-    if (usernameField && message.username && (!message.autoFill || !usernameField.value)) {
+    if (usernameField && message.username) {
       setNativeValue(usernameField, message.username);
       filledUsername = true;
     }
@@ -105,10 +105,6 @@ chrome.runtime.onMessage.addListener((message, _sender, sendResponse) => {
     return true;
   }
 });
-
-setTimeout(() => {
-  chrome.runtime.sendMessage({ type: "PADLOCK_AUTO_FILL_ON_LOAD" }).catch(() => {});
-}, 250);
 
 // --- Save-prompt on form submit -------------------------------------------
 
